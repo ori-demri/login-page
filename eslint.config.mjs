c5937fc6b29efef4aa0c938766decda826c68380
ori-demri <yod021103@gmail.com>
2024-04-19 09:28:06 +0300

    fix eslint comments

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,25 @@
-import globals from "globals";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+/* eslint-disable max-len */
+// eslint-disable-next-line import/no-extraneous-dependencies
+import globals from 'globals';
+// eslint-disable-next-line import/no-extraneous-dependencies, import/extensions
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
 
-import path from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-import pluginJs from "@eslint/js";
+import path from 'path';
+import { fileURLToPath } from 'url';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { FlatCompat } from '@eslint/eslintrc';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import pluginJs from '@eslint/js';
 
 // mimic CommonJS variables -- not needed if using CommonJS
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended});
+const filename = fileURLToPath(import.meta.url);
+// eslint-disable-next-line no-underscore-dangle
+const dirname = path.dirname(filename);
+const compat = new FlatCompat({ baseDirectory: dirname, recommendedConfig: pluginJs.configs.recommended });
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default [
-  {languageOptions: { globals: globals.browser }},
-  ...compat.extends("standard-with-typescript"),
+  { languageOptions: { globals: globals.browser } },
+  ...compat.extends('standard-with-typescript'),
   pluginReactConfig,
 ];
