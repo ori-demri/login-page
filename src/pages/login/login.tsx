3c08d5fe9f49b72048ab2b5d33cfb76dd150e14e
ori-demri <yod021103@gmail.com>
2024-04-20 20:49:43 +0300

    final frontend login page

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import './login.css';
+import person from '../../assets/person.png';
 
 function Login() {
   const [username, setUsername] = useState('');
@@ -11,28 +13,37 @@ function Login() {
   };
 
   return (
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
+    <div className="background">
+      <img className="person-img" src={person} alt="Person" />
+      <h1>
+        <span style={{ fontWeight: 'bold' }}>User</span>
+        {' '}
+        <span style={{ fontWeight: 'normal' }}>Login</span>
+      </h1>
+      <form className="form-container" onSubmit={handleSubmit}>
+        <div className="inputs">
+          <div>
+            <input
+              placeholder="Username"
+              type="text"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <input
+              placeholder="Password"
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
         </div>
-        <div>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit">Login</button>
+        <button className="login-btn" type="submit">Log In</button>
       </form>
     </div>
   );
